ab490ee1d90d189d1eb568e275b3d4c6db913c69
victoriaglavnik <92651835+victoriaglavnik@users.noreply.github.com>
2023-05-08 16:10:55 -0400

    Update spending-graph.js

--- assignment_5/js/spending-graph.js
+++ assignment_5/js/spending-graph.js
@@ -34,24 +34,6 @@ function createBarChart (data) {
         bar.style.height = dataItem.value + "px";
         bar.style.borderStyle = "ridge";
         bar.style.borderColor = dataItem.color;
-        
-        // Visual flair with CSS Level 3 (for maximum compatibility
-        bar.style.MozBoxShadow = "rgba(128, 128, 128, 0.75) 0px 7px 12px";
-        bar.style.WebkitBoxShadow = "rgba(128, 128, 128, 0.75) 0px 7px 12px";
-        bar.style.boxShadow = "rgba(128, 128, 128, 0.75) 0px 7px 12px";
-        bar.style.MozBorderRadiusTopleft = "8px";
-        bar.style.WebkitBorderTopLeftRadius = "8px";
-        bar.style.borderTopLeftRadius = "8px";
-        bar.style.MozBorderRadiusTopright = "8px";
-        bar.style.WebkitBorderTopRightRadius = "8px";
-        bar.style.borderTopRightRadius = "8px";
-        bar.style.backgroundImage = 
-            "-moz-linear-gradient(" + dataItem.color + ", black)";
-        bar.style.backgroundImage = 
-            "-webkit-gradient(linear, 0% 0%, 0% 100%," +
-            "color-stop(0, " + dataItem.color + "), color-stop(1, black))";
-        bar.style.backgroundImage =
-            "linear-gradient(" + dataItem.color + ", black)";
 
         // Recall that positioning properties are treated *relative*
         // to the corresponding sides of the containing element.
@@ -62,32 +44,34 @@ function createBarChart (data) {
         // width as space between columns.
         barPosition += (barWidth * 2);
     }
-    
     return chart;
 
-   
-    
 };
 
-
 window.onload = function () {
-    var data = [{ color: "#829b9e", value: 2005.00 },
-          { color: "#474747", value: 1471.31 },
-          { color: "#c9dcde", value: 892.86 },
-          { color: "#c9cbde", value: 531.60 },
-          { color: "#4d7d60", value: 1646.00 },
-          { color: "#4d538f", value: 179.52 }
+    var data = [
+         {color: "#829b9e", value: 2005.00},
+         {color: "#474747", value: 1471.31},
+         {color: "#c9dcde", value: 892.86},
+         {color: "#c9cbde", value: 531.60 },
+         {color: "#4d7d60", value: 1646.00 },
+         {color: "#4d538f", value: 179.52 }
 
     ];
     
     var chart = createBarChart(data);
     document.body.appendChild(chart);
-    
-document.getElementById("reset").addEventListener("click", function() {
-    for (j = 0; j < data.length; j++){
-         bar.style.height = "0px";
-     }
 
+    document.getElementById("reset").addEventListener("click", function(event) {
+        event.preventDefault()
+        
+        chartBars = document.querySelectorAll('.chartBars');
+
+        chartBars.forEach(bar => {
+            bar.style.height = 0;
+        })   
+        data.forEach(bar => {
+           bar.value = 0;
+    })
+})
 };
-
- })
